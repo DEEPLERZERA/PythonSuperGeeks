faddfb9a4f656f3825a57fb3befc2fd400a12896
Daniel Borges Valentim <daniel.valentim@conductor.com.br>
2022-05-31 14:38:41 -0300

    First version

---
+++ testeaula1.py
@@ -0,0 +1,118 @@
+nome, idade, sexo, altura = "Maria", 21, "F", 1.79
+
+num = 10
+x = 10
+a = 10
+print("Ola")
+print(nome)
+print(idade)
+print(nome * 3)
+
+
+#teste = int(input("Digite uma coisa: "))1515
+#print(type(teste))
+#print(teste * 3)
+#teste = float(input("Digite sua idade: "))
+#print(teste * 3)
+
+#nome = 50
+#print("Bem-vinda ", nome)  Concatena de forma mais eficiente permitindo até inteiro com string
+
+#numero = 5
+#print(type(numero))
+#print(str(numero))
+
+
+#ATRIBUIÇÕES ESPECIAIS
+
+numero = 5
+#print(numero)
+#numero += 5
+#print(numero)
+#numero -= 5 
+#numero *= 5
+#numero /= 5
+
+print((5+2) * 2)
+
+
+#print(11%2)  Calcula resto da divisão
+
+#OPERADORES LÓGICOS
+
+print(numero==1)  #Verificando se igualdade é verdadeira
+print(numero != 6)
+
+#AND e OR
+
+print(numero == 0 and numero == 10)  #Fazendo mais verificações com AND E OR
+
+print(numero == 0 or numero == 5)
+
+print(numero !=0 and numero == 5)
+
+#NOT
+print(not(numero==1))
+
+#OPERADORES DE MEMBRO
+
+lista = [1,8,"Ana","João"]  #Array
+
+print(1 in lista) #Verificando se está na lista
+print(10 in lista)
+print("Ana" in lista)
+print("Ana" not in lista)
+
+#OPERADORES RELACIONAIS
+
+numero = 10
+
+print(numero > 10)
+print(numero >= 10)
+
+print(numero < 5)
+print(numero <= 10)
+
+print(numero > 0 and numero < 20)
+
+print(numero == 10 and numero < 5)
+print(numero == 10 and numero <= 10)
+
+print(numero != 10 or numero > 10)
+print(numero != 10 or numero >= 10)
+
+#OPERADOR DE IDENTIDADE
+
+a = 50
+
+b = 60
+
+c = 60
+
+print(b is c)
+
+print(a is b)
+
+print(not(a is b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
