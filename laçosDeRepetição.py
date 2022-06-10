e636294f10d2a2c134784656506c1e808bd23efb
Daniel Borges Valentim <daniel.valentim@conductor.com.br>
2022-06-10 10:32:46 -0300

    Versão 10/06

---
+++ laçosDeRepetição.py
@@ -0,0 +1,78 @@
+for x in range(10):   #Cria laço de repetição
+    print("oi")    #Imprime na tela
+
+
+
+#nomes = ["Jéssica", "Marcelo", "Lívia", "Rauny", "Natália"]   #Criando array
+
+
+#for x in nomes:   #Percorre a array nomes pegando seus itens e atribuindo a x
+    #print("Bom dia", x)    #Imprime na tela
+
+
+#for x in range(3):  #Cria laço de repetição
+   #print(x)   #Imprime na tela valores de x
+
+
+
+#for x in range(5, 16):  #Cria laço de repetição passando parametros iniciais e finais
+    #print(x)   #Imprime na telinha
+
+
+#for x in range(1, 21, 3):  #Agora criamos laço de repetição com parâmetros e regra de incremento
+    #print(x)  #Imprimimos na tela
+
+
+
+#for x in range(10, 0, -1):  #Criando laço de repetição estilo contagem regressiva
+    #print(x)  #Imprime na tela
+
+
+
+for y in range(5):  #Criando laço de repetição
+    print("OI", y)  #Imprime na tela
+else:  #Caso acabe o for faça
+    print("Eu disse 'oi' 5 vezes!!!!!")  #Imprime na tela
+
+
+
+#num  = int(input("Digite um número: "))  #Atribui valor a variável
+
+#for x in range(11):   #Cria laço de repetição
+     #print(num, "x", x, "=", x * num)   #Imprime na tela o número dado pelo usuário multiplicado por x e isso fica igual a x * num
+
+
+
+#soma = 0  #Atribui valor a variável
+
+#for x in range(5):  #Laço de repetição
+    #num = int(input("Digite um número: "))  #Recebendo inputs do usuário e atribuindo na variável
+    #soma = soma + num  #Somando os números
+
+#print("A soma de todos os números é:", soma) #Ao sair do laço de repetição usuário vai ver na tela, valor da soma
+
+
+
+for x in range(5):  #Cria  laço de repetição
+    numero = int(input("Digite um número: "))   #Atribui valor a variável
+
+    if x == 0:  #Faz verificador
+        maior = numero  #Atribui valor a variável
+
+    if numero > maior:  #Faz outra verificação
+        maior = numero  #Atribui valor a variável
+print("O maior número é:", maior) #Imprime na tela         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
