86aaad597067279fcd4899ec2427432279eede72
Daniel Borges Valentim <daniel.valentim@conductor.com.br>
2022-06-24 10:17:52 -0300

    VersãoBoa 24/06

--- Ex43.py
+++ Ex43.py
@@ -2,9 +2,9 @@ soma = 0   #Declara variáveis
 soma2 = 0
 n = int(input("Digite um número: "))  #Pega input
 if soma > n:
-    while soma < n:  #Cria laço de repetição
-        soma += 1  #Cria incremento
-        soma2 = soma2 + soma #Faz soma
+    while soma > n:  #Cria laço de repetição
+        n += 1  #Cria incremento
+        soma2 = soma2 + n #Faz soma
     print(soma2)  #Imprime na tela
 else:    
 
