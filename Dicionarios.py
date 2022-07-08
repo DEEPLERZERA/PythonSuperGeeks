2d92bbe19cfef43366e6582bfed3f7c598df159b
Daniel Borges Valentim <daniel.valentim@conductor.com.br>
2022-07-08 11:31:32 -0300

    Versão 08/07

---
+++ Dicionarios.py
@@ -0,0 +1,11 @@
+#Dicionarios
+
+lista = ['ana', 5, 1.8]
+
+tupla = ('ana', 5, 1.8)
+
+matrizTeste = (('ana', 5, 1.8),
+                ['ana', 5, 1.8])
+
+
+aluno = {'nome' : "Natalia", 'idade' : 47, 'estado' : "SP"}  #Criando dicionario
--- Matriz.py
+++ Matriz.py
@@ -16,6 +16,74 @@ print(alunos3[2])    #Imprime vetor na tela
 print(alunos3[0][1]) #Imprime na tela de acordo com vetores
 
 
+nome = ["Daniel"]  #Criando variáveis
+
+sexo = ["m"]
+
+telefone = ["11956281384"]
+
+cadastro = [] #Criando lista
+
+cadastro.extend([nome, sexo, telefone])  #Adicionando itens a lista
+
+print(cadastro)
+
+nome[0] = "Gustavo"  #Alterando variáveis
+
+sexo[0] = "m"
+
+telefone[0] = "9876543321"
+
+print(nome)
+
+print(cadastro)
+
+cadastro.extend([nome, sexo, telefone])  #Repetindo dados na lista
+
+print(cadastro) 
+
+nome[0] = "Daniel" #Mudando variável nome novamente
+
+print(cadastro)
+
+
+nomeNovo = []  #Criando arrays
+nomeAntigo = []
+
+nomeAntigo.insert(0, cadastro[0][0])  #Adicionando cadastro[0][0] ao nomeAntigo
+
+nomeNovo.append(cadastro[0])  #Adicionando cadastro[0] a nomeNovo
+
+nome[0] = "Kauan"  #Alterando variáveis
+
+print(f"Nome novo: {nomeNovo}")  #Imprimindo na tela resultados
+print(f"Nome antigo: {nomeAntigo}")
+print(f"Cadastro: {cadastro}")
+
+
+telefone.clear()  #Apagando telefone
+print(cadastro)
+
+
+
+nome2 = cadastro[0]
+
+print(nome)
+print(nome2)
+print(nomeNovo)
+print(nomeAntigo)
+
+
+print(id(nome))  #Imprime id das variáveis
+print(id(nome2))
+
+print(id(nomeAntigo))
+print(id(nomeNovo))
+
+
+
+
+
 
 
 
